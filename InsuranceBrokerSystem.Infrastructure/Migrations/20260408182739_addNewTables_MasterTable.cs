eba70a852eb29a242af968e67af764f26dc51ad3
israa <ea6965046@gmail.com>
2026-04-08 20:29:23 +0200

    migration

---
+++ InsuranceBrokerSystem.Infrastructure/Migrations/20260408182739_addNewTables_MasterTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsuranceBrokerSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNewTables_MasterTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
